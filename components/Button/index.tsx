6a495e963f5065e3216ecee91f83fe407b58a10c
SeanVenz <seanvenz.quijano@cit.edu>
2023-11-04 17:50:40 +0800

    ui ux for form button

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -15,19 +15,4 @@ function ButtonSkeleton() {
   return <button className="button-skeleton">&nbsp;</button>;
 }
 
-function SecondaryButton({ text }: any) {
-  return (
-    <div className="secondary-button">
-      <button>
-        <span className="circle1"></span>
-        <span className="circle2"></span>
-        <span className="circle3"></span>
-        <span className="circle4"></span>
-        <span className="circle5"></span>
-        <span className="text">Submit</span>
-      </button>
-    </div>
-  );
-}
-
-export { MainButton, ButtonSkeleton, SecondaryButton };
+export { MainButton, ButtonSkeleton };
--- components/Form/index.tsx
+++ components/Form/index.tsx
@@ -1,11 +1,11 @@
-"use client";
 import React, { useState } from "react";
 import "./index.scss";
-import { MainButton } from "..";
-import { sendTransaction } from "@/app/lib/actions";
-import { SecondaryButton } from "../Button";
-import close from '../../public/icons8-close-50.png'
+import close from "../../public/icons8-close-50.png";
 import Image from "next/image";
+import * as Web3 from "@solana/web3.js";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import base58 from "bs58";
+import { storage } from "@/app/lib/firebase";
 
 function BusinessModal({
   isOpen,
@@ -19,13 +19,73 @@ function BusinessModal({
   const [businessType, setBusinessType] = useState("");
   const [businessName, setBusinessName] = useState("");
   const [businessDescription, setBusinessDescription] = useState("");
-  const [businessOwner, setBusinessowner] = useState("");
+  const [businessOwner, setBusinessOwner] = useState("");
   const [location, setLocation] = useState("");
   const [image, setImage] = useState(null);
+  const [submitButtonText, setSubmitButtonText] = useState("Submit");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
   const handleImageChange = (e: any) => {
     const selectedImage = e.target.files[0];
     setImage(selectedImage);
+  };
+
+  const handleSubmit = async (e: any, image: any) => {
+    e.preventDefault();
+    setSubmitButtonText("Submitting");
+    setIsButtonDisabled(true);
+    try {
+      if (wallet && image) {
+        const storageRef = ref(storage, `solana-images/${image.name}`);
+        await uploadBytes(storageRef, image);
+        const imageUrl = await getDownloadURL(storageRef);
+
+        const message = {
+          businessType,
+          businessName,
+          businessDescription,
+          imageUrl,
+          businessOwner,
+          location,
+        };
+        const messageBuffer = Buffer.from(JSON.stringify(message), "utf-8");
+        const connection = new Web3.Connection(Web3.clusterApiUrl("devnet"));
+        const publicKey = new Web3.PublicKey(wallet);
+        const base58DecodedPK = base58.decode(
+          "2Xui4ZSPyGUYMQWnronf6j7jwFLnLup8dbe2dH1fgPrHZ1FArPD274JDcSCBsyVRMNPfvEqPemxzNZe41BLeCiLf"
+        );
+        const signer = Web3.Keypair.fromSecretKey(base58DecodedPK);
+        const programId = new Web3.PublicKey(
+          "G3QrQ1JmrFhRDZyruxp84HH1WWjAmp74PFkFGWSjSYpU"
+        );
+
+        const instruction = new Web3.TransactionInstruction({
+          keys: [
+            {
+              pubkey: publicKey,
+              isSigner: true,
+              isWritable: false,
+            },
+          ],
+          data: messageBuffer,
+          programId: programId,
+        });
+
+        const transaction = await Web3.sendAndConfirmTransaction(
+          connection,
+          new Web3.Transaction().add(instruction),
+          [signer]
+        );
+
+        console.log(message);
+        setSubmitButtonText("Submitted");
+        setIsButtonDisabled(true);
+      }
+    } catch (error) {
+      console.log(error);
+      setSubmitButtonText("");
+      setIsButtonDisabled(false);
+    }
   };
 
   return (
@@ -33,22 +93,19 @@ function BusinessModal({
       <div className={`modal-content ${isOpen ? "" : "close"}`}>
         <div className="form-container">
           <div className="close-form">
-            <Image onClick={onClose} src = {close} alt ="Close" width={50} height={50}></Image>
-          </div> 
+            <Image
+              onClick={onClose}
+              src={close}
+              alt="Close"
+              width={50}
+              height={50}
+            ></Image>
+          </div>
           <form
             className="form"
-            onSubmit={(e) =>
-              sendTransaction(
-                e,
-                wallet,
-                image,
-                businessType,
-                businessName,
-                businessDescription,
-                businessOwner,
-                location
-              )
-            }
+            onSubmit={(e) => {
+              handleSubmit(e, image);
+            }}
           >
             <div className="form-group">
               <label htmlFor="Type">Business Type</label>
@@ -67,7 +124,6 @@ function BusinessModal({
                 <option value="Hospitality">Hospitality</option>
               </select>
             </div>
-
             <div className="form-group">
               <label htmlFor="email">Business Name</label>
               <input
@@ -85,7 +141,7 @@ function BusinessModal({
                 name="owner"
                 id="owner"
                 type="text"
-                onChange={(e) => setBusinessowner(e.target.value)}
+                onChange={(e) => setBusinessOwner(e.target.value)}
               />
             </div>
             <div className="form-group">
@@ -117,7 +173,16 @@ function BusinessModal({
                 onChange={handleImageChange}
               />
             </div>
-            <SecondaryButton text = {"Submit"}/>
+            <div className="secondary-button">
+              <button disabled={isButtonDisabled}>
+                <span className="circle1"></span>
+                <span className="circle2"></span>
+                <span className="circle3"></span>
+                <span className="circle4"></span>
+                <span className="circle5"></span>
+                <span className="text">{submitButtonText}</span>
+              </button>
+            </div>
           </form>
         </div>
       </div>

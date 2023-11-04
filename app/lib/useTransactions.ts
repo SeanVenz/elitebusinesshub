e300d613a53d3130bde6cffd2a5ec544f1b11d7f
SeanVenz <seanvenz.quijano@cit.edu>
2023-11-04 12:24:56 +0800

    rawr

--- app/lib/useTransactions.ts
+++ app/lib/useTransactions.ts
@@ -1,26 +1,33 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import * as Web3 from "@solana/web3.js";
 
-export const useTransactions = (wallet : any) => {
+export const useTransactions = (wallet: any) => {
   const [transactions, setTransactions] = useState<any[]>([]);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchTransactions = async () => {
-      setIsLoading(true);
       if (wallet) {
         try {
+          setIsLoading(true);
           const connection = new Web3.Connection(Web3.clusterApiUrl("devnet"));
-          const programId = new Web3.PublicKey("G3QrQ1JmrFhRDZyruxp84HH1WWjAmp74PFkFGWSjSYpU");
+          const programId = new Web3.PublicKey(
+            "G3QrQ1JmrFhRDZyruxp84HH1WWjAmp74PFkFGWSjSYpU"
+          );
 
-          const signatures = await connection.getConfirmedSignaturesForAddress2(programId, {
-            limit: 100,
-          });
+          const signatures = await connection.getConfirmedSignaturesForAddress2(
+            programId,
+            {
+              limit: 100,
+            }
+          );
 
           const transactionsData = [];
           for (const signature of signatures) {
-            const transaction = await connection.getConfirmedTransaction(signature.signature);
+            const transaction = await connection.getConfirmedTransaction(
+              signature.signature
+            );
             transactionsData.push(transaction);
           }
           setTransactions(transactionsData);
--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -15,42 +15,37 @@ import Image from "next/image";
 import { playFair } from "@/app/lib/fonts";
 
 function Cards({ wallet }: { wallet: string | null }) {
-  if (wallet) {
-    const { transactions, error, isLoading } = useTransactions(wallet);
+  const { transactions, error, isLoading } = useTransactions(wallet);
 
-    // if (isLoading) {
-    //   return <CardSkeleton />;
-    // }
-
-    return (
-      <>
-        {transactions.length > 0 && (
-          <div className="transaction-parent">
-            <h2>Available Businesses:</h2>
-            <ul>
-              {transactions.map((transaction, index) => (
-                <li key={index}>
-                  <Card
-                    imgLink={formatMessage(decodeMessage(transaction)).field3}
-                    signature={decodeSignature(
-                      transaction.transaction.signature
-                    )}
-                    date={formatDate(transaction.blockTime)}
-                    messageOne={
-                      formatMessage(decodeMessage(transaction)).field1
-                    }
-                    messageTwo={
-                      formatMessage(decodeMessage(transaction)).field2
-                    }
-                  />
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </>
-    );
+  // You can conditionally render a loading state if needed
+  if (isLoading) {
+    return <CardSkeleton />;
   }
+
+  return (
+    <>
+      {transactions.length > 0 && (
+        <div className="transaction-parent">
+          <h2>Available Businesses:</h2>
+          <ul>
+            {transactions.map((transaction, index) => (
+              <li key={index}>
+                <Card
+                  imgLink={formatMessage(decodeMessage(transaction)).field3}
+                  signature={decodeSignature(
+                    transaction.transaction.signature
+                  )}
+                  date={formatDate(transaction.blockTime)}
+                  messageOne={formatMessage(decodeMessage(transaction)).field1}
+                  messageTwo={formatMessage(decodeMessage(transaction)).field2}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Cards;
